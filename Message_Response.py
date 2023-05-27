5e45bba5d27582973a536636a35716c64e4cdb65
Arin Sharma <111631186+slayerrr12@users.noreply.github.com>
2023-05-27 22:07:02 +0530

    Update Message_Response.py and added 2 added two commands
    Added a reaction to a specific message: When a message containing "wave" (case-insensitive) is received, the bot reacts with a waving hand emoji.
    
    Help Command: When a user sends the command !help, the bot responds with a help message listing available commands.

--- Message_Response.py
+++ Message_Response.py
@@ -1,27 +1,48 @@
 from dotenv import dotenv_values
+import discord
+from discord import Intents
+
 import Data.Messages.MessagesData as Messages
 
-import discord
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
+# Load environment variables from .env file
 env_vars = dotenv_values('.env')
+
+# Configure Discord client with intents
+intents = Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_ready():
     print('Wave is Started')
 
+
 @client.event
 async def on_message(message):
-    
+    # Ignore messages sent by the bot itself
     if message.author == client.user:
         return
 
+    # Check if the received message is a hello message
     if message.content.lower() in Messages.Hello_messages:
-        Reply=Messages.HelloReply()
-        await message.channel.send(Reply)    
+        Reply = Messages.HelloReply()
+        await message.channel.send(Reply)
 
-    
+    # React to a specific message
+
+    # Add a reaction if the message content is "wave"
+    if message.content.lower() == 'wave':
+        await message.add_reaction('👋')
+
+    # Additional features
+
+    # Help Command
+    if message.content.lower() == '!help':
+        help_message = "I am Wave Bot. Here are some available commands:\n" \
+                       "`!hello` - Say hello to the bot\n" \
+                       "`!help` - Show this help message"
+        await message.channel.send(help_message)
+
+
 client.run(env_vars['BotToken'])
