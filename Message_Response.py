2c151d4183fe70a70eed2469d98430c9eddee640
Harsh <harshshrivastava554@gmail.com>
2023-05-27 23:36:10 +0530

    Messages Reaction and Middle Finger Reply

--- Message_Response.py
+++ Message_Response.py
@@ -1,5 +1,5 @@
 from dotenv import dotenv_values
-import Data.Messages.MessagesData as Messages
+import Data.MessagesData as Messages
 
 import discord
 intents = discord.Intents.default()
@@ -15,7 +15,6 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    
     if message.author == client.user:
         return
 
@@ -23,5 +22,36 @@ async def on_message(message):
         Reply=Messages.HelloReply()
         await message.channel.send(Reply)    
 
+    if message.content == 'cool':
+        await message.add_reaction('\U0001F60E')
+
+    if message.content == '\U0001F595':
+        await message.add_reaction('\U0001F92C')
+
+@client.event
+async def on_message_edit(before,after):
+    #Message Edit log
+    if before.content != after.content:
+        await before.channel.send(
+            f'>>> '
+            f'{before.author} edit a message. \n'
+            f'Before: {before.content}\n'
+            f'After: {after.content}'
+        )
     
+@client.event
+async def on_reaction_add(reaction,user):
+    if reaction.emoji == '\U0001F595':
+        await reaction.message.channel.send(f'Fuck You {user}')
+
+    #Reaction logs
+    if user == client.user:
+        pass
+    else:
+        await reaction.message.channel.send(
+            f'>>> '
+            f'{user} reacted with {reaction.emoji}'
+        )
+
+
 client.run(env_vars['BotToken'])
